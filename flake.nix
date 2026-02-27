fd3d31e0c60ec7c7624199212605fc19290534ab
Fukaya Temma <23367170+pranc1ngpegasus@users.noreply.github.com>
2026-02-27 17:48:32 +0900

    feat: setup nix flake (#1)

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    git-hooks.url = "github:cachix/git-hooks.nix";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      git-hooks,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+        pre-commit-check = git-hooks.lib.${system}.run {
+          src = ./.;
+          hooks = {
+            nixfmt.enable = true;
+          };
+        };
+      in
+      {
+        checks = {
+          inherit pre-commit-check;
+        };
+
+        devShells.default = pkgs.mkShell {
+          inherit (pre-commit-check) shellHook;
+          buildInputs =
+            (with pkgs; [
+              nixfmt
+            ])
+            ++ pre-commit-check.enabledPackages;
+        };
+      }
+    );
+}
